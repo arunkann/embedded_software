4cc7c0d41f330baf97c8dd68cbf07c0f97584fd0
Arun Kannan <arunkann@gmail.com>
2019-09-28 07:57:13 -0700

    Fix bugs in process.c with semaphore
    Signed-off-by: Arun Kannan <arunkann@gmail.com>

--- process.c
+++ process.c
@@ -9,6 +9,11 @@
 #define E2O_SEM "/e2o_sem"
 #define O2E_SEM "/o2e_sem"
 
+/* Semaphore is an integer whose value never falls below 0.
+ * sem_post increments semaphore.
+ * sem_wait decrements semaphore, if current value > 0. If 0, it blocks.
+ */
+
 void* even_process(void* data)
 {
     int limit = *(int*)data;
@@ -16,25 +21,15 @@ void* even_process(void* data)
     int val;
     sem_t *e2o_sem, *o2e_sem;
 
-    o2e_sem = sem_open(O2E_SEM, O_RDWR, 0644, 0);
-    e2o_sem = sem_open(E2O_SEM, O_RDWR, 0644, 0);
+    o2e_sem = sem_open(O2E_SEM, O_RDWR);
+    e2o_sem = sem_open(E2O_SEM, O_RDWR);
 
     if ((o2e_sem == SEM_FAILED) || (e2o_sem == SEM_FAILED)) {
         printf("semaphore init error\n");
         exit(EXIT_FAILURE);
     }
 
-    /* wait for odd process to start */
-    status = sem_wait(o2e_sem);
-    if (status)
-        printf("even: sem_wait ret error %d\n", errno);
-
-    /* hand off to odd process to start for loop prints */
-    status = sem_post(e2o_sem);
-    if (status)
-        printf("even: sem_post ret error %d\n", errno);
-
-    for (volatile int i = 0; i < limit; i=i+2) {
+    for (int i = 0; i < limit; i=i+2) {
         /* wait for odd process */
         status = sem_wait(o2e_sem);
         if (status)
@@ -48,6 +43,10 @@ void* even_process(void* data)
         if (status)
             printf("even: sem_post ret error %d\n", errno);
     }
+
+    sem_close(o2e_sem);
+    sem_close(e2o_sem);
+    exit(0);
 }
 
 void* odd_process(void* data)
@@ -57,35 +56,21 @@ void* odd_process(void* data)
     int status;
     sem_t *e2o_sem, *o2e_sem;
 
-    o2e_sem = sem_open(O2E_SEM, O_RDWR, 0644, 0);
-    e2o_sem = sem_open(E2O_SEM, O_RDWR, 0644, 0);
+    o2e_sem = sem_open(O2E_SEM, O_RDWR);
+    e2o_sem = sem_open(E2O_SEM, O_RDWR);
 
     if ((o2e_sem == SEM_FAILED) || (e2o_sem == SEM_FAILED)) {
         printf("semaphore init error\n");
         exit(EXIT_FAILURE);
     }
 
-    /* signal even process */
-    status = sem_post(o2e_sem);
-    if (status)
-        printf("odd: sem_post ret error %d\n", errno);
-
-    /* get control back to start for loop */
-    status = sem_wait(e2o_sem);
-    if (status)
-        printf("odd: sem_wait ret error %d\n", errno);
-
     /* this post kicks off even process to start printing */
     status = sem_post(o2e_sem);
     if (status)
         printf("odd: sem_post ret error %d\n", errno);
 
-    for (volatile int i = 1; i < limit; i=i+2) {
+    for (int i = 1; i < limit; i=i+2) {
         /* wait for even process */
-        status = sem_getvalue(o2e_sem, &val);
-        if (status)
-            printf("odd: sem_getvalue ret error %d\n", errno);
-
         status = sem_wait(e2o_sem);
         if (status)
             printf("odd: sem_wait ret error %d\n", errno);
@@ -98,6 +83,10 @@ void* odd_process(void* data)
         if (status)
             printf("odd: sem_post ret error %d\n", errno);
     }
+
+    sem_close(o2e_sem);
+    sem_close(e2o_sem);
+    exit(0);
 }
 
 int main()
@@ -107,6 +96,7 @@ int main()
     int status;
     int val;
     sem_t *e2o_sem, *o2e_sem;
+    int rc;
 
     o2e_sem = sem_open(O2E_SEM, O_CREAT, 0644, 0);
     e2o_sem = sem_open(E2O_SEM, O_CREAT, 0644, 0);
@@ -116,16 +106,13 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    /* Ensure initial value is 0 */
+#if 0
+    /* Just sanity check */
     sem_getvalue(o2e_sem, &val);
-    for (int i=0; i<val; i++)
-        sem_wait(o2e_sem);
-    sem_getvalue(o2e_sem, &val);
-
+    printf("o2e_sem value %d\n", val);
     sem_getvalue(e2o_sem, &val);
-    for (int i=0; i<val; i++)
-        sem_wait(e2o_sem);
-    sem_getvalue(e2o_sem, &val);
+    printf("e2o_sem value %d\n", val);
+#endif
 
     /* save parent pid to distinguish from child */
     parent_pid = getpid();
@@ -139,15 +126,18 @@ int main()
     if (parent_pid == getpid() && (even_pid < 0 || odd_pid < 0))
         printf("Failed to fork process\n");
 
-    /* wait for children to finish */
-    if (parent_pid == getpid())
+    if (parent_pid == getpid()) {
+        /* wait for children to finish */
         while (wait(&status) > 0);
 
-    if (parent_pid == getpid()) {
     	sem_close(e2o_sem);
     	sem_close(o2e_sem);
-    	sem_unlink("/odd_to_even_sem");
-    	sem_unlink("/even_to_odd_sem");
+    	rc = sem_unlink(E2O_SEM);
+        if (rc)
+            printf("o2e sem unlink failed\n");
+    	rc = sem_unlink(O2E_SEM);
+        if (rc)
+            printf("o2e sem unlink failed\n");
     }
 
     return 0;
