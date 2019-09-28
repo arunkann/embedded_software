e34e4c0b70bee0fb9ddfce2a144bdefbf5960b40
Arun Kannan <arunkann@gmail.com>
2019-09-28 19:48:01 +0000

    Fix bugs in cond_wait usage
    Signed-off-by: Arun Kannan <arunkann@gmail.com>

--- threads.c
+++ threads.c
@@ -1,53 +1,57 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <pthread.h>
+
+/* pthread_cond_wait() unlocks the mutex before it sleeps.
+   When it is woken up (due to signal), it reacquires the mutex - which may require waiting again.
+   So, the thread would continue only after a) signaling and b) mutex unlocked by signaling thread.
+
+   Because of the above behavior, its recommended to:
+   a) signal and wait within mutex lock and unlock.
+   b) wait within if condition, preferably a while loop on a condition variable.
+	This condition variable ensures waiting only when needed.
+ */
 
 pthread_mutex_t mutex =  PTHREAD_MUTEX_INITIALIZER; 
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-
-void handshake(void) 
-{
-    pthread_mutex_lock(&mutex);
-    pthread_cond_signal(&cond);
-    pthread_mutex_unlock(&mutex);
-}
+bool even = true;
 
 void* even_thread(void* data)
 {
     int limit = *(int*)data;
 
-    /* wait for odd thread to start */
-    pthread_cond_wait(&cond, &mutex);
-
     for (int i = 0; i < limit; i=i+2) {
+        printf("%d\n", i);
+
         pthread_mutex_lock(&mutex);
-        printf("%d ", i);
+	even = false;
         pthread_cond_signal(&cond);
         pthread_mutex_unlock(&mutex);
-        pthread_cond_wait(&cond, &mutex);
-    }
 
-    /* signal odd thread to complete */
-    handshake();
+        pthread_mutex_lock(&mutex);
+	while (!even)
+        	pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_unlock(&mutex);
+    }
 }
 
 void* odd_thread(void* data)
 {
     int limit = *(int*)data;
 
-    /* signal even thread */
-    handshake();
-
-    /* odd thread waits for even to start printing */
-    pthread_cond_wait(&cond, &mutex);
-
     for (int i = 1; i < limit; i=i+2) {
         pthread_mutex_lock(&mutex);
-        printf("%d ", i);
+	while (even)
+        	pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_unlock(&mutex);
+
+        printf("%d\n", i);
+
+        pthread_mutex_lock(&mutex);
+	even = true;
         pthread_cond_signal(&cond);
         pthread_mutex_unlock(&mutex);
-        pthread_cond_wait(&cond, &mutex);
     }
-
 }
 
 int main()
